2c319969abb2b5cc7abe83106ed0e5ecc5d41d8a
Ryp <ryp.sqrt@gmail.com>
2013-10-10 20:39:11 +0000

    Added: GNU GPL v3 License

--- src/Shader/ShaderManager.cpp
+++ src/Shader/ShaderManager.cpp
@@ -1,3 +1,20 @@
+/*
+ * This file is part of GLShaderDev.
+ *
+ * GLShaderDev is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GLShaderDev is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GLShaderDev.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 #include "ShaderManager.h"
 
 ShaderManager::ShaderManager(unsigned sizeLimit)
